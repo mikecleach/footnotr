9634bca213b58a5e73077584de9a019c0a51ac0c
Mike Leach <nderzero@gmail.com>
2013-04-24 01:02:57 -0500

    trying to remove submodule

---
+++ Footnotr/CommentsView.h
@@ -0,0 +1,13 @@
+//
+//  CommentsView.h
+//  Footnotr
+//
+//  Created by Sam Homer on 4/23/13.
+//  Copyright (c) 2013 Sam Homer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentsView : UIView
+
+@end
---
+++ Footnotr/CommentsView.m
@@ -0,0 +1,31 @@
+//
+//  CommentsView.m
+//  Footnotr
+//
+//  Created by Sam Homer on 4/23/13.
+//  Copyright (c) 2013 Sam Homer. All rights reserved.
+//
+
+#import "CommentsView.h"
+
+@implementation CommentsView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
